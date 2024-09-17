7f111a8e931174a45e767d31df44c7e5bbb144e7
Naman Kudesia <namankudesia@gmail.com>
2024-09-17 21:23:30 +0530

    My repo

---
+++ awesome-portfolio-websites/assets/js/skills.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    AOS.init({
+        duration: 1200,
+    });
+
+    // Optional: Add any additional JS functionality here if needed
+});
